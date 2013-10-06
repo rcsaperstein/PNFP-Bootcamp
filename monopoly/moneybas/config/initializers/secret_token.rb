7d44ab6e8533fcc5128179ae062396fc3ddd4c74
rcsaperstein <rcsaperstein@gmail.com>
2013-10-06 15:19:50 -0700

    Create moneybags app

---
+++ monopoly/moneybas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moneybas::Application.config.secret_key_base = '07d4b496b62260696f4d62529259598f906e9dbbbb4840bedc467ce92d122d7c48b63ebd89649f98f767fc77732b26e24a2e75f2feb7cc5b17ef8d1e8d6826e5'
